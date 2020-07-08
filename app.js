5a60be859b404d7c2d6a9caaa020bcebe6d97eee
Dino <dinokunovac@gmail.com>
2020-07-08 18:33:51 -0400

    added shiny toggle functionality for sprites

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const pokeContainer = document.querySelector('.pokemon-container')
 const gen1 = 'https://pokeapi.co/api/v2/pokemon?limit=151'
+const shinySwitch = document.querySelector('input[type="checkbox"]');
 
 
 // fetch  gen1 pokemon
@@ -22,6 +23,7 @@ const fetchGen1PokemonData = (pokemon) => {
 
 // display pokemon with data retrieved
 const renderPokemon = pokeData => {
+
     const pokeDiv = document.createElement('div')
     const pokeName = document.createElement('h3')
     const pokeImg = document.createElement('img')
@@ -32,12 +34,22 @@ const renderPokemon = pokeData => {
     pokeDiv.className = 'pokemon' 
     pokeName.innerText = pokeData.name
     pokeImg.src = pokeData.sprites['front_default']
+    pokeImg.className = 'images'
     pokeNum.innerText = `Pokedex #${pokeData.id}`
     pokeType.innerText = `Type: ${pokeData.types[0].type.name}`
+
+    shinySwitch.addEventListener('change', e => {
+        if (e.target.checked) {
+            pokeImg.src = pokeData.sprites['front_shiny']
+        } else {
+            pokeImg.src = pokeData.sprites['front_default']
+        }
+    })
 
     pokeDiv.append(pokeName, pokeImg, pokeNum, pokeType)
     pokeContainer.appendChild(pokeDiv)
 }
 
 
-// fetchGen1Pokemon()
+
+fetchGen1Pokemon()
