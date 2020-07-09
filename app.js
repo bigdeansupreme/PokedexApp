aec0739adafe8d28096000232c8f76bfd9fe660f
Dino <dinokunovac@gmail.com>
2020-07-09 11:30:05 -0400

    added event listeners for buttons

--- app.js
+++ app.js
@@ -1,43 +1,52 @@
+
 const pokeContainer = document.querySelector('.pokemon-container')
-const gen1 = 'https://pokeapi.co/api/v2/pokemon?limit=151'
-const shinySwitch = document.querySelector('input[type="checkbox"]');
+const gen1Url = 'https://pokeapi.co/api/v2/pokemon?limit=151'
+const gen2Url = 'https://pokeapi.co/api/v2/pokemon/?limit=100&offset=151'
+const gen3Url = 'https://pokeapi.co/api/v2/pokemon/?limit=135&offset=251'
+const shinySwitch = document.querySelector('input[type="checkbox"]')
+const gen1Btn = document.getElementById('gen-one')
+const gen2Btn = document.getElementById('gen-two')
+const gen3Btn = document.getElementById('gen-three')
+// const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
 
 
-// fetch  gen1 pokemon
-const fetchGen1Pokemon = () => {
-    fetch(gen1)
+// fetch list of pokemon
+const fetchPokemon = (url) => {
+    pokeContainer.innerHTML = ''
+    fetch(url)
     .then(res => res.json())
     .then(allPokemon => {
-        allPokemon.results.forEach(pokemon => fetchGen1PokemonData(pokemon))
+        allPokemon.results.forEach(pokemon => fetchPokemonData(pokemon))
     })
 }
 
+
 // fetch each pokemon's data
-const fetchGen1PokemonData = (pokemon) => {
+const fetchPokemonData = (pokemon) => {
     let pokeInfoUrl = pokemon.url
     fetch(pokeInfoUrl)
     .then(res => res.json())
     .then(pokeData => renderPokemon(pokeData))
-
+    
 }
 
 // display pokemon with data retrieved
 const renderPokemon = pokeData => {
-
+    
     const pokeDiv = document.createElement('div')
     const pokeName = document.createElement('h3')
     const pokeImg = document.createElement('img')
     const pokeNum = document.createElement('p')
     const pokeType = document.createElement('p')
-
+    
     pokeDiv.dataset.id = pokeData.id
     pokeDiv.className = 'pokemon' 
     pokeName.innerText = pokeData.name
-    pokeImg.src = pokeData.sprites['front_default']
+    // pokeImg.src = pokeData.sprites['front_default']
     pokeImg.className = 'images'
     pokeNum.innerText = `Pokedex #${pokeData.id}`
     pokeType.innerText = `Type: ${pokeData.types[0].type.name}`
-
+    
     shinySwitch.addEventListener('change', e => {
         if (e.target.checked) {
             pokeImg.src = pokeData.sprites['front_shiny']
@@ -45,11 +54,35 @@ const renderPokemon = pokeData => {
             pokeImg.src = pokeData.sprites['front_default']
         }
     })
-
+    
     pokeDiv.append(pokeName, pokeImg, pokeNum, pokeType)
     pokeContainer.appendChild(pokeDiv)
 }
 
+//event listeners
+
+gen1Btn.addEventListener('click', () => {
+    fetchPokemon(gen1Url)
+})
+
+gen2Btn.addEventListener('click', () => {
+    fetchPokemon(gen2Url)
+})
+
+gen3Btn.addEventListener('click', () => {
+    fetchPokemon(gen3Url)
+})
 
 
-fetchGen1Pokemon()
+
+
+
+
+
+
+
+
+
+
+
+
